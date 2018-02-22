c54494027211834c6466aebce3cda6e1cd05469b
Qya Ahmad <raqayyah@msn.com>
2018-02-22 14:17:03 -0500

    hof

--- hof.js
+++ hof.js
@@ -20,10 +20,11 @@ var people = [
 // person in the `people` array. Assign the returned array to a variable
 // called `peopleAges`.
 // Type your solution immediately below this line:
-
+var peopleAges = .map(people.age)
 
 
 // #2: Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who are WDI students. Assign the returned array
 // to a variable called `wdiStudents`.
 // Type your solution immediately below this line:
+var wdiStudents = .filter(people, coarse = wdi )
